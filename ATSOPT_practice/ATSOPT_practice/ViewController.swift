cc4b89577a76f6715bbdfc07e37f8617f18c7111
JURI <80569323+juri123123@users.noreply.github.com>
2025-04-05 13:18:55 +0900

    initial commit

---
+++ ATSOPT_practice/ATSOPT_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ATSOPT_practice
+//
+//  Created by 최주리 on 4/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
