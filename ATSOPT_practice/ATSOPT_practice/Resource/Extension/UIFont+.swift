39907da59c97dad9ca39d45f81f7fc1b77d3d6b8
JURI <80569323+juri123123@users.noreply.github.com>
2025-04-10 17:30:26 +0900

    add: text style

--- ATSOPT_practice/ATSOPT_practice/Resource/Extension/UIFont+.swift
+++ ATSOPT_practice/ATSOPT_practice/Resource/Extension/UIFont+.swift
@@ -27,4 +27,32 @@ extension UIFont {
         
         return customFont
     }
+    
+    @nonobjc class var head1: UIFont {
+        return UIFont.font(.pretendardBold, ofSize: 25)
+    }
+    
+    @nonobjc class var subhead1: UIFont {
+        return UIFont.font(.pretendardBold, ofSize: 18)
+    }
+    
+    @nonobjc class var subhead2: UIFont {
+        return UIFont.font(.pretendardRegular, ofSize: 16)
+    }
+    
+    @nonobjc class var subhead3: UIFont {
+        return UIFont.font(.pretendardBold, ofSize: 15)
+    }
+    
+    @nonobjc class var subhead4: UIFont {
+        return UIFont.font(.pretendardBold, ofSize: 14)
+    }
+    
+    @nonobjc class var subhead5: UIFont {
+        return UIFont.font(.pretendardRegular, ofSize: 14)
+    }
+    
+    @nonobjc class var info12: UIFont {
+        return UIFont.font(.pretendardRegular, ofSize: 12)
+    }
 }
