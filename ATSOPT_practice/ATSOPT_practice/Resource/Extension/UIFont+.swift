6245014617e055c3f4e39c74e5aaaf382d26bafc
JURI <80569323+juri123123@users.noreply.github.com>
2025-04-10 17:11:23 +0900

    에셋 추가, 폰트 설정

---
+++ ATSOPT_practice/ATSOPT_practice/Resource/Extension/UIFont+.swift
@@ -0,0 +1,30 @@
+//
+//  UIFont+.swift
+//  ATSOPT_practice
+//
+//  Created by 최주리 on 4/5/25.
+//
+
+import UIKit
+
+enum FontName: String {
+    case pretendardBlack = "Pretendard-Black"
+    case pretendardBold = "Pretendard-Bold"
+    case pretendardExtraBold = "Pretendard-ExtraBold"
+    case pretendardExtraLight = "Pretendard-ExtraLight"
+    case pretendardLight = "Pretendard-Light"
+    case pretendardMedium = "Pretendard-Medium"
+    case pretendardRegular = "Pretendard-Regular"
+    case pretendardSemiBold = "Pretendard-SemiBold"
+    case pretendardThin = "Pretendard-Thin"
+}
+
+extension UIFont {
+    static func font(_ style: FontName, ofSize size: CGFloat) -> UIFont {
+        guard let customFont = UIFont(name: style.rawValue, size: size) else {
+            return UIFont.systemFont(ofSize: size)
+        }
+        
+        return customFont
+    }
+}
